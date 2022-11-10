5610d3c72abb75dfc22fcec172049adfe6ddcccf
Ivan <soloveii@mydigicode.com>
2022-11-10 14:07:57 +0200

    test noda

--- server-1/app.js
+++ server-1/app.js
@@ -1,9 +1,9 @@
+const { Client } = require('pg')
 const http = require('http');
 const url = require('url');
 const express = require('express')
 const app = express()
 
-const { Client } = require('pg')
 
 const conn = new Client({
   host: "mouse.db.elephantsql.com",
@@ -12,6 +12,9 @@ const conn = new Client({
   password: "FNMMLJNkOZn9P8BKKWLtol773KRhVf22",
   port: "5432"
 })
+
+let query="SELECT * FROM users";
+
 conn.connect(function (err) {
     if (err) {
         return console.error("Ошибка: " + err);
@@ -21,26 +24,36 @@ conn.connect(function (err) {
     }
   });
 
-//   let query="SELECT * FROM users";
 
 //   conn.query(query, (err, result, field) =>{
 //     console.log(err);
 //     console.log(result);
-//     //  console.log(field);
 // });
 
+// async function main() {
+//   const conn = await Client.createConnection({
+//     host: "mouse.db.elephantsql.com",
+//     user: "gtelkgeo",
+//     database: "gtelkgeo",
+//     password: "FNMMLJNkOZn9P8BKKWLtol773KRhVf22",
+//     port: "5432"
+//   });
+
+// }
+
+// http.createServer(function (req, res) {
+//   let urlParts = url.parse(req.url);
+//   console.log('----------------');
+//   console.log(urlParts.pathname);
+// }).listen(4200);
+
 app.get('/users', (req, res) => {
-  conn.query('SELECT * FROM users', function (err, result) {
-    if (err) {
-        return console.error("Ошибка: " + err);
-    }
-    else {
-      console.log(res);
-    }
-    res.send(rows)
-    console.log(result);
+  res.send('Hi, Noda works!')
+  conn.query(query, (err, result, field) =>{
+    console.log(err);
+    console.log(result)
   })
-})
+});
 
 // client.end(function (err) {
 //   if (err) {
@@ -50,3 +63,5 @@ app.get('/users', (req, res) => {
 //       console.log("Off");
 //   }
 // });
+
+app.listen(my-shop-angular-one.vercel.app)
